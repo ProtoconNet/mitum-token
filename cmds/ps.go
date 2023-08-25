8e5fc4eeb1e436e959ce9419bf6aa4929dccc70d
wyuinche <wyuinche.mido@gmail.com>
2023-08-25 12:31:01 +0900

    fix: wrong implementation of processors map

--- cmds/ps.go
+++ cmds/ps.go
@@ -4,11 +4,11 @@ import (
 	"context"
 
 	currencycmds "github.com/ProtoconNet/mitum-currency/v3/cmds"
-	"github.com/ProtoconNet/mitum-currency/v3/operation/processor"
+	currencyprocessor "github.com/ProtoconNet/mitum-currency/v3/operation/processor"
 	"github.com/ProtoconNet/mitum-currency/v3/types"
 	"github.com/ProtoconNet/mitum-token/operation/token"
 
-	// "github.com/ProtoconNet/mitum-token/operation/processor"
+	"github.com/ProtoconNet/mitum-token/operation/processor"
 	"github.com/ProtoconNet/mitum2/base"
 	"github.com/ProtoconNet/mitum2/isaac"
 	"github.com/ProtoconNet/mitum2/launch"
@@ -27,16 +27,18 @@ type processorInfo struct {
 func POperationProcessorsMap(pctx context.Context) (context.Context, error) {
 	var isaacParams *isaac.Params
 	var db isaac.Database
+	var opr *currencyprocessor.OperationProcessor
+	var set *hint.CompatibleSet[isaac.NewOperationProcessorInternalFunc]
 
 	if err := util.LoadFromContextOK(pctx,
 		launch.ISAACParamsContextKey, &isaacParams,
 		launch.CenterDatabaseContextKey, &db,
+		currencycmds.OperationProcessorContextKey, &opr,
+		launch.OperationProcessorsMapContextKey, &set,
 	); err != nil {
 		return pctx, err
 	}
 
-	set := hint.NewCompatibleSet[isaac.NewOperationProcessorInternalFunc](1 << 9)
-	opr := processor.NewOperationProcessor()
 	err := opr.SetCheckDuplicationFunc(processor.CheckDuplication)
 	if err != nil {
 		return pctx, err
